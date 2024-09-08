d5f0f3c99b9300fc3af2f876abd70fbcf9c7f3e4
Deepak Sharma <ce.deepaksharma@gmai.com>
2024-09-08 17:36:43 +0530

    fix router error

--- src/main.jsx
+++ src/main.jsx
@@ -31,9 +31,9 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        errorElement: <Home />,
+        element: <Home />,
         path: "/",
-        element: <ErrorPage />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "",
